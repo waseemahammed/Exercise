616302bcc7d3dcb6d27caad7d1a3dd2d6ef7515f
waseemahammed <waseemahammed96@gmail.com>
2018-03-15 14:03:27 +0530

    first commit

--- string.py
+++ string.py
@@ -1,22 +1,19 @@
 ListOfNumbers = []
-
-def getInputFromUSer(str):
+sumOfAlphabet
+def getInputFromUSer():
     str = raw_input("Enter a string here : ")
     ConvertToLowerCase = str.lower()
     trimmedString = ConvertToLowerCase.replace(" ", "")
-return trimmed
 
-def convertIntoNumber(int)
-for i in trimmedString:
-    number = ord(i) - 96
-    ListOfNumbers.append(number)
-    print(ListOfNumbers)
-    sumOfAlphabet = sum(ListOfNumbers)
-    print(sumOfAlphabet)
-return sumOfAlphabet
-
-while sumOfAlphabet>9:
-    rem = sumOfAlphabet % 10
-    sumOfAlphabet = (sumOfAlphabet / 10) + rem
-        
-print(sumOfAlphabet)
+def convertIntoNumber():
+    for i in trimmedString:
+        number = ord(i) - 96
+        ListOfNumbers.append(number)
+        print(ListOfNumbers)
+        sumOfAlphabet = 0
+        sumOfAlphabet = sum(ListOfNumbers)
+        print(sumOfAlphabet)
+    while sumOfAlphabet>9:
+        rem = sumOfAlphabet % 10
+        sumOfAlphabet = (sumOfAlphabet / 10) + rem    
+        print(sumOfAlphabet)
